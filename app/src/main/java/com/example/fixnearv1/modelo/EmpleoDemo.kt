e48873750df6221935e39dcd9ac1a23632db0ab0
Maximiliano Uribe Quintero <m.uribe23@info.uas.edu.mx>
2026-05-24 22:24:00 -0700

    Extraer el data class EmpleoDemo a su propio paquete de modelo

---
+++ app/src/main/java/com/example/fixnearv1/modelo/EmpleoDemo.kt
@@ -0,0 +1,9 @@
+package com.example.fixnearv1.modelo
+
+data class EmpleoDemo(
+    val empresa: String,
+    val puesto: String,
+    val salario: String,
+    val horario: String,
+    val distancia: String
+)
